ec1c9041f506e8ac4dc292d3132e6006d817c6c8
EpicErick <e_delfin_23@yahoo.com>
2016-02-22 00:48:38 -0700

    new commit

--- src/codeevalchallenges/RemoveCharacters_moderate.java
+++ src/codeevalchallenges/RemoveCharacters_moderate.java
@@ -1,11 +1,11 @@
 /*
 
-CHALLENGE DESCRIPTION:
+ CHALLENGE DESCRIPTION:
 
-Write a program which removes specific characters from a string.
+ Write a program which removes specific characters from a string.
 
-Input example:  how are you, abc
-Output example: how re you
+ Input example:  how are you, abc
+ Output example: how re you
 
  */
 package codeevalchallenges;
@@ -17,35 +17,50 @@ package codeevalchallenges;
 public class RemoveCharacters_moderate {
 
     public static void main(String[] args) {
-
-        String line1 = "how are you, abc";
         
+        String line1 = "how are you, abc";
+
         String line2 = "hello world, def";
         
-        System.out.println(splitAndRemove(line2));
+        /*
+        The way the strings are formatted are the way it 
+        had to be done on Codeeval, so I had no choice :/
+        
+        These are just examples, this could also be done
+        with scanner, bufferedreader (for console input)
+        */        
+        
+        splitAndRemove(line1);
+        splitAndRemove(line2);
 
     }
 
-    public static String splitAndRemove(String x) {
-        
+    public static void splitAndRemove(String x) { //splits input sentence and characters and removes characters in sentence
+
         String[] seperate = x.split(", ");
-        String sentence = seperate[0];
+        char[] lettersToRead = seperate[0].toCharArray();
         char[] lettersToReplace = seperate[1].toCharArray();
-        
-        String output = null;
-        
-        for (int i = 0; i < sentence.length(); i++) {
-            
+
+        for (int i = 0; i < lettersToRead.length; i++) {
+
             for (int j = 0; j < lettersToReplace.length; j++) {
-                
-                if (sentence.charAt(i) == lettersToReplace[j]) {
-                    output = sentence.replace(sentence.charAt(i), '\0');
+
+                if (lettersToRead[i] == lettersToReplace[j]) {
+                    lettersToRead[i] = '\0';
+                    break;
                 }
-                
+
             }
-            
+
         }
-        return output;
+        charArrayToString(lettersToRead);
     }
 
+    public static void charArrayToString(char[] array) { //prints a char array
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println("");
+    }
 }
