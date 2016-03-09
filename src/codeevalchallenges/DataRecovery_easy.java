479d242335e202bec752228285299cddf1221cdd
EpicErick <e_delfin_23@yahoo.com>
2016-03-08 23:05:22 -0700

    Revert "still need to fix the null position to string in output ON DataRecovery_easy"
    This reverts commit 339b0faa56cb4144a0fee9a7f7315e9dfe52ebf7.

--- src/codeevalchallenges/DataRecovery_easy.java
+++ src/codeevalchallenges/DataRecovery_easy.java
@@ -21,7 +21,7 @@ public class DataRecovery_easy {
     
     public static void main(String[] args) {
         
-        String line = "programs Manchester The written ran Mark 1952 1 in Autocode from;6 2 1 7 5 3 11 4 8 9";
+        String line = "2000 and was not However, implemented 1998 it until;9 8 3 4 1 5 7 2";
         String[] splitParts = line.split(";");
         
         
