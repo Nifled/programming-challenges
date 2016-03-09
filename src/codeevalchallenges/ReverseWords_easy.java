726f4fc1ff739eb30ebfd85cb7029c899d07370a
EpicErick <e_delfin_23@yahoo.com>
2016-03-08 23:42:57 -0700

    commit ReverseWords challenge

---
+++ src/codeevalchallenges/ReverseWords_easy.java
@@ -0,0 +1,39 @@
+/*
+CHALLENGE DESCRIPTION:
+
+Write a program which reverses the words in an input sentence.
+
+EXAMPLE: 
+
+input: Hello World
+output: World Hello
+ */
+package codeevalchallenges;
+
+/**
+ *
+ * @author Erick
+ */
+public class ReverseWords_easy {
+    
+    public static void main(String[] args) {
+        
+        String test = "hello world";
+        
+        System.out.println(reverseLine(test));
+        
+    }
+    
+    public static String reverseLine(String line) {
+        
+        String[] parts = line.split(" ");
+        int n = parts.length;
+        String output = "";
+        
+        for (int i = n-1; i >= 0; i--) {
+            output += parts[i] + " ";
+        }
+        return output;
+    }
+    
+}
