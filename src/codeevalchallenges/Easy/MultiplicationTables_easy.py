b9fa9ab2b582deaacd4ccea50dbac0a42fb85b84
EpicErick <e_delfin_23@yahoo.com>
2016-03-22 23:53:53 -0700

    Multiplication tables in Python :D

---
+++ src/codeevalchallenges/Easy/MultiplicationTables_easy.py
@@ -0,0 +1,12 @@
+# https://www.codeeval.com/open_challenges/23/
+# MULTIPLICATION TABLES
+
+# CHALLENGE DESCRIPTION:
+
+# Print out the grade school multiplication table upto 12*12.
+
+limit = 12
+for x in range(1,limit+1):
+	for y in range(1,limit+1):
+		print('{:4}'.format(x*y), end="")
+	print("")
