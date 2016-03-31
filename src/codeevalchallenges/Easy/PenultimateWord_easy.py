fcb5c6303359b5ecf9f03ec61b5d57dcdf120e40
EpicErick <e_delfin_23@yahoo.com>
2016-03-30 21:23:26 -0700

    Add Penultimate Word in Py

---
+++ src/codeevalchallenges/Easy/PenultimateWord_easy.py
@@ -0,0 +1,8 @@
+# https://www.codeeval.com/open_challenges/92/
+# Penultimate Word
+
+# CHALLENGE DESCRIPTION:
+
+# Write a program which finds the next-to-last word in a string.
+test = "some line with text"
+print(test.strip().split()[-2])
