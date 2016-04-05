47395dbb73a3949fea8d34963838a2913613fb08
EpicErick <e_delfin_23@yahoo.com>
2016-04-04 21:46:17 -0700

    Add Even Numbers.py

---
+++ src/codeevalchallenges/Easy/EvenNumbers_easy.py
@@ -0,0 +1,9 @@
+# https://www.codeeval.com/open_challenges/100/
+# Even Numbers 
+
+# CHALLENGE DESCRIPTION:
+
+# Write a program which checks input numbers and determines whether a number is even or not.
+test = "2936"
+
+print(1 if int(test)%2 == 0 else 0)
