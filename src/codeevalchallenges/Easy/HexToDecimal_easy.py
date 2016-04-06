4d215c3971cc78c2cd263253c5821eb2633a3d62
EpicErick <e_delfin_23@yahoo.com>
2016-04-05 22:59:40 -0700

    Add Hex to Decimal.py

---
+++ src/codeevalchallenges/Easy/HexToDecimal_easy.py
@@ -0,0 +1,9 @@
+# https://www.codeeval.com/open_challenges/67/
+# Hex To Decimal
+# 
+# CHALLENGE DESCRIPTION:
+
+# You will be given a hexadecimal (base 16) number. Convert it into decimal (base 10).
+test = "9f"
+
+print(int(test, 16))
