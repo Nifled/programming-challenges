084d592d266103396e921b456472719b98108182
Erick <e_delfin_23@yahoo.com>
2016-05-06 02:11:04 -0700

    Modified Rightmost Char_easy.py

--- src/codeevalchallenges/Easy/RightmostChar_easy.py
+++ src/codeevalchallenges/Easy/RightmostChar_easy.py
@@ -6,16 +6,9 @@
 # You are given a string 'S' and a character 't'. Print out the position 
 # of the rightmost occurrence of 't' (case matters) in 'S' or -1 if there 
 # is none. The position to be printed out is zero based.
-test = "Oat bT6MJHoyjGWpHYoj8dYE3U3fVry9NRiWK8unxXI6aieWj6kJom4wmgiPqBbva60ZU, " #Just an example
+test = "IgqMe0kLZq74sDjfOD2jDcWcjttODlyRixSpzg3HiXS,c"
+test = test.strip()
+
 phrase = test.split(',')[0]
 chary = test.split(',')[1]
-
-index = []
-for x in range(len(phrase)-1,-1,-1):
-    if phrase[x] == chary:
-        index.append(x)
-        break
-if len(index) == 0:
-    print(-1)
-else: 
-    print(index[0])
+print(str(phrase.rfind(chary)))
