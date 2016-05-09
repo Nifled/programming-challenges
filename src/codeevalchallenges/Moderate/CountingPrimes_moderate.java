c1cd3c118a70e08316ad5358fdb7c77f480d326c
Erick <e_delfin_23@yahoo.com>
2016-05-09 16:02:22 -0700

    Modified Counting Primes.java, made it faster

--- src/codeevalchallenges/Moderate/CountingPrimes_moderate.java
+++ src/codeevalchallenges/Moderate/CountingPrimes_moderate.java
@@ -8,41 +8,33 @@ between N and M (both inclusive)
 package codeevalchallenges.Moderate;
 
 /**
- *
+ *||
  * @author Erick
  */
 public class CountingPrimes_moderate {
 
     public static void main(String[] args) {
-        
         String line = "20,30"; // Codeeval example
         String[] separator = line.split(",");
-        
+
         int from = Integer.parseInt(separator[0]);
         int to = Integer.parseInt(separator[1]);
-        
+
         int countResult = 0;
         for (int i = from; i <= to; i++) {
-            if (isPrime(i)) {
-                countResult++;
-            }
+            if (isPrime(i)) countResult++;
         }
         System.out.println(countResult);
     }
-    
+
     // Returns true if number IS prime
     public static boolean isPrime(int x) {
-        if (x >= 2) {
-            int root = (int) Math.sqrt(x);
-
-            int count = 0;
-            for (int i = 1; i < root + 1; i++) {
-                if (x % i == 0) {
-                    count++;
-                }
-            }
-            return count <= 1;
-        } 
-        return false;
+        if (x < 2) return false;
+        if (x > 2 && x%2 == 0) return false;
+        int root = (int) Math.sqrt(x);
+        for (int i=3; i < root+1; i+=2){
+            if (x%i == 0) return false;
+        }
+    return true;
     }
 }
