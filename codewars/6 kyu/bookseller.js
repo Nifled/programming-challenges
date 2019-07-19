2668fa3344a3b4a3174dc9ade8e7b3060e4028d6
Erick Delfin <erickdelfinmolina@gmail.com>
2019-07-19 00:47:38 -0700

    Refactor bookseller kata

--- codewars/6 kyu/bookseller.js
+++ codewars/6 kyu/bookseller.js
@@ -1,31 +1,27 @@
 // https://www.codewars.com/kata/54dc6f5a224c26032800005c
 
 /**
- * b = ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"]
- * c = ["A", "B", "C", "W"]
+ * Fisrt letter of `b` represents category to be counted.
+ *
+ * booklist = ["CBART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"]
+ * categories = ["A", "B", "C", "W"]
  *
  * Should return "(A : 0) - (B : 114) - (C : 70) - (W : 0)"
  */
 
 const stockList = (booklist, categories) => {
-  if (booklist.length <= 0) return "";
-  const stocks = {};
+  if (!booklist.length) return "";
 
-  booklist.forEach(book => {
-    const letter = book[0];
+  const stocks = booklist.reduce((arr, book) => {
+    const [letter] = book;
     const stock = Number(book.split(" ")[1]);
 
     if (categories.includes(letter)) {
-      stocks.hasOwnProperty(letter)
-        ? (stocks[letter] += stock)
-        : (stocks[letter] = stock);
+      arr[letter] ? (arr[letter] += stock) : (arr[letter] = stock);
     }
-  });
 
-  return categories
-    .map(cat => {
-      if (stocks.hasOwnProperty(cat)) return `(${cat} : ${stocks[cat]})`;
-      return `(${cat} : 0)`;
-    })
-    .join(" - ");
+    return arr;
+  }, {});
+
+  return categories.map(cat => `(${cat} : ${stocks[cat] || 0})`).join(" - ");
 };
